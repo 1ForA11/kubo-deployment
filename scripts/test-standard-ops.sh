24dff6ea86dacfaaf520ea40e7b068a52bf05745
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-29 11:50:41 +1000

    all standard ops files being tested

--- scripts/test-standard-ops.sh
+++ scripts/test-standard-ops.sh
@@ -18,6 +18,15 @@ test_standard_ops() {
       check_interpolation "worker-haproxy.yml" "-l example-vars-files/worker-haproxy.yml"
       check_interpolation "worker-haproxy.yml" "-o iaas/vsphere/worker-haproxy.yml" "-v worker_haproxy_ip_addresses=10.10.10.10" "-l example-vars-files/worker-haproxy.yml"
       check_interpolation "worker-haproxy.yml" "-o iaas/openstack/worker-haproxy.yml" "-v worker_haproxy_ip_addresses=10.10.10.10" "-l example-vars-files/worker-haproxy.yml"
+
+      # HTTP proxy options
+      check_interpolation "add-http-proxy.yml" "-v http_proxy=10.10.10.10:8000"
+      check_interpolation "add-https-proxy.yml" "-v https_proxy=10.10.10.10:8000"
+      check_interpolation "add-no-proxy.yml" "-v no_proxy=localhost,127.0.0.1"
+
+      check_interpolation "addons-spec.yml" "-v authorization-mode=rbac" "-v addons-spec={}"
+      check_interpolation "use-runtime-config-bosh-dns.yml"
+
     popd > /dev/null # operations
   popd > /dev/null
   exit $exit_code
